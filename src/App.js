4f6da93ee3acea3e1c643a561e3a4f0e69beb873
LWHITE <LWHITE@HBS-LWHITE>
2022-11-14 20:11:27 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <button class="btn btn-danger">Submit</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "jquery";
+import "propper/index";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
